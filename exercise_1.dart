7ee456f55d4634270404a43885106177f969b15a
AidaSadigova <132117419+AidaSadigova@users.noreply.github.com>
2023-04-29 05:13:31 -0800

    Add files via upload

---
+++ exercise_1.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+main(){
+  print('Please enter your name:');
+  String? user_name = stdin.readLineSync()!;
+  print('Please enter your age:');
+  int user_age = int.parse(stdin.readLineSync()!);
+  int year=0;
+  print('Istifadeci adi $user_name -dir ve yasi $user_age -dir.');
+  while(user_age!=100){
+    user_age=user_age+1;
+    year=year+1;
+    
+  }
+  print('Istifadecinin 100yasinin tamam olmasina $year il qalib.');
+}
